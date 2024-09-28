c55c46814ee7dd0a3c9f06baf19faae629750132
Piotr Tyrakowski <verscup@gmail.com>
2024-09-29 01:00:26 +0200

    better type ImageData

--- parent_app/src/app/list/page.tsx
+++ parent_app/src/app/list/page.tsx
@@ -1,12 +1,17 @@
 // app/page.tsx (or any page you'd like to use)
 'use client'
 import { useEffect, useState } from 'react';
+import { convertISOToDateTime } from '@/utils/dateTimeConverter';
 
 type ImageData = {
   _id: string;
   buffer: string; // Base64-encoded string
   submittedAt: string;
+  weight: string; //weight --
+  face: string; // 1 if happy face, 0 if unhappy face
 };
+
+
 
 const ImageGallery = () => {
   const [images, setImages] = useState<ImageData[]>([]);
@@ -43,7 +48,8 @@ const ImageGallery = () => {
               alt="Submitted"
               style={{ width: '200px', height: '200px', objectFit: 'cover' }}
             />
-            <p>Submitted at: {new Date(image.submittedAt).toLocaleString()}</p>
+            <p>Submitted at: {new Date(convertISOToDateTime(image.submittedAt)).toLocaleString()}</p>
+            <p>Weight: {image.weight}</p>
           </li>
         ))}
       </ul>
