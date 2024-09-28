9d4f36cf487e80049c72d79096baf548ae3ba085
Wiktor Frątczak <01171649@pw.edu.pl>
2024-09-28 20:53:24 +0200

    apply styles for first 3 boxes

--- parent_app/src/app/page.tsx
+++ parent_app/src/app/page.tsx
@@ -7,28 +7,28 @@ const HomePage = () => {
       <div className="grid grid-cols-2 gap-4">
 
         {/* Poop Icon and Number */}
-        <div className="col-span-2 flex flex-col items-center justify-center bg-white p-6 rounded-lg shadow-md">
-          <h1 className="text-5xl font-bold">1</h1>
-          <span className="text-6xl">💩</span>
+        <div className="col-span-2 flex flex-row gap-x-2 items-center justify-center bg-white p-2 rounded-[1rem] shadow-md h-[16rem]">
+          <h1 className="text-[15rem] font-bold">1</h1>
+          <span className="text-[10rem]">💩</span>
         </div>
 
         {/* Recommended Article Section */}
-        <div className="col-span-1 bg-white p-6 rounded-lg shadow-md flex flex-col items-center">
+        <div className="col-span-1 bg-white p-6 rounded-[1rem] shadow-md flex flex-col items-center h-[12rem]">
           <span className="text-3xl">🍎</span>
           <p className="text-center mt-4">Polecajka artykuł</p>
         </div>
 
 
         {/* Recommended Article Section */}
-        <div className="col-span-1 bg-white p-6 rounded-lg shadow-md flex flex-col items-center">
+        <div className="col-span-1 bg-white p-6 rounded-[1rem] shadow-md flex flex-col items-center h-[12rem]">
           <h2 className="text-sm text-gray-600 mb-2">To dobre źródło błonnika</h2>
           <p className="text-center mt-4">Polecajka artykuł</p>
         </div>
 
-        
+
 
         {/* Weight Chart */}
-        <div className="col-span-2 bg-white p-6 rounded-lg shadow-md">
+        <div className="col-span-2 bg-white p-6 rounded-[1rem] shadow-md">
           <h2 className="text-sm text-gray-600 mb-2">Waga</h2>
           <div className="h-32 w-full bg-blue-200 flex items-center justify-center">
             {/* Replace with actual chart */}
@@ -37,7 +37,7 @@ const HomePage = () => {
         </div>
       {/* </div> */}
 
-        <div className="col-span-2 bg-white p-6 rounded-lg shadow-md">
+        <div className="col-span-2 bg-white p-6 rounded-[1rem] shadow-md">
           <h2 className="text-sm text-gray-600 mb-2">Waga</h2>
           <div className="h-32 w-full bg-blue-200 flex items-center justify-center">
             {/* Replace with actual chart */}
@@ -45,7 +45,7 @@ const HomePage = () => {
           </div>
         </div>
         </div>
-      
+
 
 
       {/* Bottom Navigation */}
