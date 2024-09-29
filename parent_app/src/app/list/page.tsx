5a398b0dd89764b45b1fa3a2b90a7862aded49cf
Piotr Tyrakowski <verscup@gmail.com>
2024-09-29 03:29:43 +0200

    server checking for 5 second

--- parent_app/src/app/list/page.tsx
+++ parent_app/src/app/list/page.tsx
@@ -1,59 +1,30 @@
-// app/page.tsx (or any page you'd like to use)
 'use client'
-import { useEffect, useState } from 'react';
+import useSWR from 'swr';
 import Card from '../../components/Card';
-import { convertISOToTime } from '@/utils/dateTimeConverter';
-import { convertISOToDate } from '@/utils/dateTimeConverter';
-
+import { convertISOToTime, convertISOToDate } from '@/utils/dateTimeConverter';
 
 type ImageData = {
   _id: string;
-  buffer: string; // Base64-encoded string
+  buffer: string;
   submittedAt: string;
-  weight: string; //weight --
-  face: string; // 1 if happy face, 0 if unhappy face
+  weight: string;
+  face: string;
 };
 
-
+const fetcher = (url: string) => fetch(url).then(res => res.json());
 
 const ImageGallery = () => {
-  const [images, setImages] = useState<ImageData[]>([]);
-  const [loading, setLoading] = useState(true);
+  const { data: images, error } = useSWR<ImageData[]>('/api/get-images', fetcher, {
+    refreshInterval: 5000, // Fetch data every 5 seconds
+  });
 
-  useEffect(() => {
-    const fetchImages = async () => {
-      try {
-        const response = await fetch('/api/get-images');
-        const data = await response.json();
-        setImages(data);
-      } catch (error) {
-        console.error('Error fetching images:', error);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchImages();
-  }, []);
-
-  if (loading) {
-    return <p className="text-black">Loading images...</p>;
-  }
+  if (error) return <p className="text-black">Failed to load images.</p>;
+  if (!images) return <p className="text-black">Loading images...</p>;
 
   return (
     <div className='min-h-screen bg-gray-100 p-4'>
       <h1 className="text-black">Image Gallery</h1>
       <ul style={{ listStyleType: 'none', padding: 0 }}>
-        {/* {images.map((image) => (
-          <li key={image._id} style={{ marginBottom: '20px' }}>
-            <img
-              src={`data:image/png;base64,${image.buffer}`}
-              alt="Submitted"
-              style={{ width: '200px', height: '200px', objectFit: 'cover' }}
-            />
-            <p>Submitted at: {new Date(image.submittedAt).toLocaleString()}</p>
-          </li> 
-        ))} */}
         {images.map((image) => (
           <Card 
             key={image._id}
@@ -61,8 +32,8 @@ const ImageGallery = () => {
             waga={image.weight} 
             godzina={convertISOToTime(image.submittedAt)} 
             data={convertISOToDate(image.submittedAt)} 
-            face={'1'} 
-            />
+            face={image.face} 
+          />
         ))}
       </ul>
     </div>
