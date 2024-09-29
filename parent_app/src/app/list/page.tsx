a4626b010327deca299ee87945f51ae430109488
Zajaczkowskim <zajac.mich@wp.pl>
2024-09-29 06:17:42 +0200

    reverse list order

--- parent_app/src/app/list/page.tsx
+++ parent_app/src/app/list/page.tsx
@@ -26,6 +26,12 @@ const ImageGallery = () => {
       try {
         const response = await fetch('/api/get-images');
         const data = await response.json();
+
+        data.sort((a: ImageData, b: ImageData) => {
+          return new Date(b.submittedAt).getTime() - new Date(a.submittedAt).getTime();
+        });
+
+
         setImages(data);
       } catch (error) {
         console.error('Error fetching images:', error);
@@ -47,16 +53,6 @@ const ImageGallery = () => {
       {/* <h1 className="text-black">Image Gallery</h1> */}
       <p className=" text-4xl font-bold text-gray-800 mb-4">Image Gallery</p>
       <ul style={{ listStyleType: 'none', padding: 0 }}>
-        {/* {images.map((image) => (
-          <li key={image._id} style={{ marginBottom: '20px' }}>
-            <img
-              src={`data:image/png;base64,${image.buffer}`}
-              alt="Submitted"
-              style={{ width: '200px', height: '200px', objectFit: 'cover' }}
-            />
-            <p>Submitted at: {new Date(image.submittedAt).toLocaleString()}</p>
-          </li> 
-        ))} */}
         {images.map((image) => (
           <Card 
             key={image._id}
